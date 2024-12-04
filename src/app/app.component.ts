faf1e384d7e41fa769f7c9f8ad352453eb2d0dd4
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-12-04 16:00:46 +0700

    integrasi package dan implement tailwind with flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'assesment_gracia_visi_pratama';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
